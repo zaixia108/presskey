5e96b0b29c9ec279d188e7feb82537ed743e9d54
xvbowen2012 <xvbowen2012@gmail.com>
2022-04-20 00:26:44 +0800

    re01

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='presskeyforgame',
+    version='0.0.0.1',
+    packages=[''],
+    url='https://github.com/xvbowen2012',
+    license='MIT',
+    author='zaixia108',
+    author_email='xvbowen2012@gmail.com',
+    description=''
+)
